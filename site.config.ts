91e3e1b722aeb5bbab9a1ce602ab036281cac165
Mochammad Aziz <64318388+izoski@users.noreply.github.com>
2025-02-20 09:34:15 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Mochammad "izo" Aziz',
 
   // social usernames (optional)
-  twitter: 'azizmocha',
-  instagram: 'izo.jpg',
-  facebook: 'izo.gg',
+  twitter: 'malib_org',
+  instagram: 'malib_org',
+  facebook: 'mochammadazizlibrary',
   // newsletter: '#', // optional newsletter URL
   youtube: '@izo.mp4', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -55,10 +55,10 @@ export default siteConfig({
      {
        title: 'About',
        pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-     },
-     {
-       title: 'Contact',
-       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
      }
+    // {
+   //    title: 'Contact',
+   //    pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+   //  }
    ]
 })
